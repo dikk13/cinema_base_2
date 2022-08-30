3558a7e9610ca14f92a76bd05f338a2d58d9464e
Andrey Dolgopolov <axweil98@mail.ru>
2022-08-30 14:41:53 +0500

    Create AbstractDao and Service

---
+++ src/main/java/com/kata/cinema/base/service/AbstractServiceImpl.java
@@ -0,0 +1,53 @@
+package com.kata.cinema.base.service;
+
+import com.kata.cinema.base.dao.AbstractDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AbstractServiceImpl<PK, E>  implements AbstractService{
+
+    private final AbstractDao abstractDao;
+
+    @Autowired
+    public AbstractServiceImpl(AbstractDao abstractDao) {
+        this.abstractDao = abstractDao;
+    }
+
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public void create(Object entity) {
+
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object entity) {
+
+    }
+
+    @Override
+    public void deleteById(Object id) {
+
+    }
+
+    @Override
+    public Optional getById(Object id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existById(Object id) {
+        return false;
+    }
+}
