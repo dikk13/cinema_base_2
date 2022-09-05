40ffcce23e423a9b0caf864a2276049fbd236962
arudenok <105078359+arudenok@users.noreply.github.com>
2022-09-05 16:52:13 +0300

    genreRest

---
+++ src/main/java/com/kata/cinema/base/converter/GenreResponseDto.java
@@ -0,0 +1,12 @@
+package com.kata.cinema.base.converter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GenreResponseDto {
+    Long id;
+    String name;
+
+}
---
+++ src/main/java/com/kata/cinema/base/webapp/controllers/admin/AdminGenreRestController.java
@@ -0,0 +1,47 @@
+package com.kata.cinema.base.webapp.controllers.admin;
+
+
+import com.kata.cinema.base.converter.GenreResponseDto;
+import com.kata.cinema.base.service.abstracts.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AdminGenreRestController {
+    private final AbstractService<Long, GenreResponseDto> abstractService;
+
+
+    @Autowired
+    public AdminGenreRestController(AbstractService<Long, GenreResponseDto> abstractService) {
+        this.abstractService = abstractService;
+    }
+
+
+    @GetMapping("/api/moderator/genres")
+    public ResponseEntity<List<GenreResponseDto>> getAllGenre() {
+        List<GenreResponseDto> genres = abstractService.getAll();
+        return ResponseEntity.ok(genres);
+    }
+
+    @DeleteMapping("/api/moderator/genres/{id}")
+    public ResponseEntity<List<GenreResponseDto>> deleteGenre(@PathVariable("id") long id) {
+        abstractService.deleteById(id);
+        return ResponseEntity.ok(abstractService.getAll());
+    }
+
+    @PutMapping("/api/moderator/genres/{id}?name=")
+    public ResponseEntity<List<GenreResponseDto>> updateGenre(@RequestBody GenreResponseDto genreDto) {
+        abstractService.update(genreDto);
+        return ResponseEntity.ok(abstractService.getAll());
+    }
+
+    @PostMapping("/api/moderator/genres?name=")
+    public ResponseEntity<List<GenreResponseDto>> addNewGenre(@RequestBody GenreResponseDto genreDto) {
+        abstractService.create(genreDto);
+        return ResponseEntity.ok(abstractService.getAll());
+    }
+}
