8e00729a69075fb159c7e0ca5c870b8ad89a4120
chiOO7 <dr.chigoo@gmail.com>
2022-09-26 11:16:57 +0300

    Созданы: 1. Объекты слоя DAO для ProductionStudioMovie & ProductionStudio 2. Объекты слоя Service для ProductionStudioMovie & ProductionStudio 3. Контролллеры AdminProductionStudioRestController &    ProductionStudioRestController

---
+++ src/main/java/com/kata/cinema/base/dao/Impl/ProductionMovieStudioDaoImpl.java
@@ -0,0 +1,21 @@
+package com.kata.cinema.base.dao.Impl;
+
+import com.kata.cinema.base.dao.abstracts.ProductionMovieStudioDao;
+import com.kata.cinema.base.models.ProductionStudioMovie;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductionMovieStudioDaoImpl extends AbstractDaoImpl<Long, ProductionStudioMovie>
+        implements ProductionMovieStudioDao {
+
+    @Override
+    public ProductionStudioMovie getStudioByMovieId(Long movieId) {
+        return  entityManager.createQuery("SELECT new com.kata.cinema.base.models.ProductionStudioMovie(psm.id, " +
+                        "psm.movie, psm.studio, psm.performance) from ProductionStudioMovie psm " +
+                        "where psm.movie.id = :id", ProductionStudioMovie.class)
+                .setParameter("id", movieId)
+                .getSingleResult();
+
+//        return null;
+    };
+}
