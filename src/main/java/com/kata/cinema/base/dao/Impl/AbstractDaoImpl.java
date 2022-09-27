2fbe755b22a1a7ae3b690f7775758216969ef138
chiOO7 <dr.chigoo@gmail.com>
2022-09-28 00:25:00 +0300

    Добавлен  @SuppressWarnings("unchecked") к методам: * public List<E> getAll(); * public Optional<E> getById(PK id).

--- src/main/java/com/kata/cinema/base/dao/Impl/AbstractDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/Impl/AbstractDaoImpl.java
@@ -8,6 +8,16 @@ import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Optional;
 
+/*
+    Этот класс имеет следующие методы с подавленными предупреждениями:
+    * public List<E> getAll();
+    * public Optional<E> getById(PK id),
+    которые подавляют Unchecked cast сыроого типа в параметризованный.
+    Предупреждение возникает из-за вероятности возникновения ClassCastException при попытке приведения элемента листа
+    к некому типу данных, являющемуся E - одним из параметров класса.
+
+ */
+
 public abstract class AbstractDaoImpl<PK, E> implements AbstractDao<PK, E> {
 
     @PersistenceContext
@@ -16,6 +26,7 @@ public abstract class AbstractDaoImpl<PK, E> implements AbstractDao<PK, E> {
     private final ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
     private final Class<?> genericType = (Class<?>) type.getActualTypeArguments()[1];
 
+    @SuppressWarnings("unchecked")
     public List<E> getAll() {
         return (List<E>) entityManager.createQuery("FROM " + genericType.getName()).getResultList();
     }
@@ -37,6 +48,7 @@ public abstract class AbstractDaoImpl<PK, E> implements AbstractDao<PK, E> {
                 .setParameter("id", id).executeUpdate();
     }
 
+    @SuppressWarnings("unchecked")
     public Optional<E> getById(PK id) {
         try {
             return (Optional<E>) Optional.of(entityManager.find(genericType, id));
