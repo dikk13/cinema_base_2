15c600e2a0f706f87718be75dc39b990bc3590c0
chiOO7 <dr.chigoo@gmail.com>
2022-09-27 12:30:14 +0300

    Исправлены остальные методы

--- src/main/java/com/kata/cinema/base/dao/Impl/AbstractDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/Impl/AbstractDaoImpl.java
@@ -1,19 +1,23 @@
 package com.kata.cinema.base.dao.Impl;
 
 import com.kata.cinema.base.dao.abstracts.AbstractDao;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class AbstractDaoImpl<PK, E>  implements AbstractDao <PK, E> {
+public abstract class AbstractDaoImpl<PK, E> implements AbstractDao<PK, E> {
 
     @PersistenceContext
     protected EntityManager entityManager;
 
+    private final ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+    private final Class<?> genericType = (Class<?>) type.getActualTypeArguments()[1];
+
     public List<E> getAll() {
-        return entityManager.createQuery("FROM " + getClass()).getResultList();
+        return (List<E>) entityManager.createQuery("FROM " + genericType.getName()).getResultList();
     }
 
     public void create(E entity) {
@@ -29,22 +33,20 @@ public abstract class AbstractDaoImpl<PK, E>  implements AbstractDao <PK, E> {
     }
 
     public void deleteById(PK id) {
-        ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
-        Class<?> genericType = (Class<?>) type.getActualTypeArguments()[1];
         entityManager.createQuery("DELETE " + genericType.getName() + " WHERE id = :id")
                 .setParameter("id", id).executeUpdate();
     }
 
     public Optional<E> getById(PK id) {
         try {
-            return (Optional<E>) Optional.of(entityManager.find(getClass(), id));
+            return (Optional<E>) Optional.of(entityManager.find(genericType, id));
         } catch (Exception ex) {
             return Optional.empty();
         }
     }
 
     public boolean existById(PK id) {
-        long count = (long) entityManager.createQuery("SELECT count(e) FROM " + getClass() + " e WHERE e.id = : id")
+        long count = (long) entityManager.createQuery("SELECT count(e) FROM " + genericType.getName() + " e WHERE e.id = : id")
                 .setParameter("id", id).getSingleResult();
         return count > 0;
     }
