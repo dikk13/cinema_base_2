3bf5383a062590a9e8a54dc6ea7c412c1bbbbfff
Alla <freshvisitka@yandex.ru>
2022-10-12 11:34:36 +0300

    Исправлены замечания

--- src/main/java/com/kata/cinema/base/dao/Impl/ScoreMovieDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/Impl/ScoreMovieDaoImpl.java
@@ -4,19 +4,29 @@ import com.kata.cinema.base.dao.abstracts.ScoreMovieDao;
 import com.kata.cinema.base.dto.ScoreMovieResponseDto;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class ScoreMovieDaoImpl implements ScoreMovieDao {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<ScoreMovieResponseDto> getItemsDto(Integer currentPage, Integer itemsOnPage, Map<String, Object> parameters) {
-        return null;
+        return entityManager.createQuery("SELECT new com.kata.cinema.base.dto.ScoreMovieResponseDto" +
+                "(s.id + s.score + s.date)" + "FROM Score s", ScoreMovieResponseDto.class)
+                .setFirstResult((currentPage - 1) * itemsOnPage)
+                .setMaxResults(itemsOnPage)
+                .getResultList();
     }
 
     @Override
     public Long getResultTotal(Map<String, Object> parameters) {
-        return null;
+        return entityManager.createQuery("select count (s) from Score s", Long.class)
+                .getSingleResult();
     }
 }
--- src/main/java/com/kata/cinema/base/service/abstracts/ScoreMovieResponseDtoService.java
+++ src/main/java/com/kata/cinema/base/service/abstracts/ScoreMovieResponseDtoService.java
@@ -2,10 +2,9 @@ package com.kata.cinema.base.service.abstracts;
 
 
 import com.kata.cinema.base.models.Movie;
-import com.kata.cinema.base.models.ProductionStudio;
 import com.kata.cinema.base.models.Score;
 
 public interface ScoreMovieResponseDtoService extends AbstractService <Long, Score>{
 
-
+    Movie findMovie(Long movieId);
 }
