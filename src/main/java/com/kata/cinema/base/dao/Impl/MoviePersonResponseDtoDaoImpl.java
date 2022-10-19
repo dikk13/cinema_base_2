d805465717aa8befc8cb3904dd915e26fbf93b34
zaborskiy <zaborskiy_ai@mail.ru>
2022-10-19 21:39:40 +0300

    фиксы

---
+++ src/main/java/com/kata/cinema/base/dao/Impl/MoviePersonResponseDtoDaoImpl.java
@@ -0,0 +1,15 @@
+package com.kata.cinema.base.dao.Impl;
+
+import com.kata.cinema.base.dao.abstracts.MoviePersonResponseDtoDao;
+import com.kata.cinema.base.dto.MoviePersonResponseDto;
+
+import java.util.List;
+
+public class MoviePersonResponseDtoDaoImpl implements MoviePersonResponseDtoDao {
+
+    //TODO метод не реализован
+    @Override
+    public List<MoviePersonResponseDto> getMoviePersonResponseDtoListByMovieId(Long id) {
+        return null;
+    }
+}
