4a6779dc35863df0ab6f1a9b56e94cdddb7171ac
mowka <58746195+mowka@users.noreply.github.com>
2022-10-27 13:34:14 +0500

    tests_for_Excertion

---
+++ src/test/java/com/kata/cinema/base/webapp/controllers/unauthorized/ExcertionRestControllerTest.java
@@ -0,0 +1,61 @@
+package com.kata.cinema.base.webapp.controllers.unauthorized;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.kata.cinema.base.dto.request.ExcertionRequestDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+class ExcertionRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DatabaseSetup("/dataset.xml")
+    @DatabaseTearDown("/empty_dataset.xml")
+    void deleteExcertion() throws Exception {
+        this.mockMvc.perform(delete("/api/excertions/{id}", "100")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @DatabaseSetup("/dataset.xml")
+    @DatabaseTearDown("/empty_dataset.xml")
+    void updateExcertion() throws Exception {
+        ExcertionRequestDto excertionRequestDto = new ExcertionRequestDto("updated text");
+        this.mockMvc.perform(put("/api/excertions/{id}", "101")
+                        .content(objectMapper.writeValueAsString(excertionRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
