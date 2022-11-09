e07a1f1a9f8ee425980f10414191bcf4507d676f
cryneogetz <C:\Users\Neogetz\AppData\Roaming\The Bat!>
2022-11-09 20:26:42 +0300

    add ReactionReviewService

---
+++ src/main/java/com/kata/cinema/base/service/entity/ReactionReviewService.java
@@ -0,0 +1,6 @@
+package com.kata.cinema.base.service.entity;
+
+import com.kata.cinema.base.models.ReactionReview;
+
+public interface ReactionReviewService extends AbstractService<Long, ReactionReview> {
+}
