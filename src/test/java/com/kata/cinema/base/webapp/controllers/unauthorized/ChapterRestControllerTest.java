dd2adb459c25bbb5109b3dc09121318000bc1923
sveklenko15@gmail.com <0995658527Satan>
2022-11-21 17:01:51 +0300

    тесты добавлены

---
+++ src/test/java/com/kata/cinema/base/webapp/controllers/unauthorized/ChapterRestControllerTest.java
@@ -0,0 +1,43 @@
+package com.kata.cinema.base.webapp.controllers.unauthorized;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+@TestExecutionListeners({
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+public class ChapterRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DatabaseSetup("/dataset.xml")
+    @DatabaseTearDown("/empty_dataset.xml")
+    void getChapters() throws Exception {
+        this.mockMvc.perform(get("/api/chapters"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
