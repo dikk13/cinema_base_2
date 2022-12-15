89f30de9450eb2331a7636fc31b93ac28d87bd07
Кузнецов Д.И <kuznecov77@mail.ru>
2022-12-15 14:58:49 +0300

    commit -a -m 'Resolve "работа с адресом": добавлены тесты'

--- src/main/java/com/kata/cinema/base/dao/entity/AddressDao.java
+++ src/main/java/com/kata/cinema/base/dao/entity/AddressDao.java
@@ -2,8 +2,6 @@ package com.kata.cinema.base.dao.entity;
 
 import com.kata.cinema.base.models.Address;
 
-import java.util.Optional;
-
 public interface AddressDao extends AbstractDao<Long, Address>{
 
 
--- src/main/java/com/kata/cinema/base/dto/request/AddressRequestDto.java
+++ src/main/java/com/kata/cinema/base/dto/request/AddressRequestDto.java
@@ -1,12 +1,12 @@
 package com.kata.cinema.base.dto.request;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class AddressRequestDto {
 
     private String street;
