0e23e7023c48c8b81a1264ea84d6ea084e1b6b0d
Viktor Kollegov <rocket22@yandex.ru>
2022-12-16 10:44:07 +0300

    added a forgotten annotation, redundant field removed

--- src/main/java/com/kata/cinema/base/service/dto/impl/AwardsCeremonyResultResponseDtoServiceImpl.java
+++ src/main/java/com/kata/cinema/base/service/dto/impl/AwardsCeremonyResultResponseDtoServiceImpl.java
@@ -2,16 +2,13 @@ package com.kata.cinema.base.service.dto.impl;
 
 import com.kata.cinema.base.dao.dto.AwardsCeremonyResultResponseDtoDao;
 import com.kata.cinema.base.dto.response.AwardsCeremonyResultResponseDto;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class AwardsCeremonyResultResponseDtoServiceImpl extends PaginationDtoServiceImpl<AwardsCeremonyResultResponseDto> {
-
-    private final AwardsCeremonyResultResponseDtoDao paginationDtoDao;
-
 
     public AwardsCeremonyResultResponseDtoServiceImpl(AwardsCeremonyResultResponseDtoDao awardsCeremonyResultResponseDtoDao) {
         super(awardsCeremonyResultResponseDtoDao);
-        this.paginationDtoDao = awardsCeremonyResultResponseDtoDao;
     }
 
 }
