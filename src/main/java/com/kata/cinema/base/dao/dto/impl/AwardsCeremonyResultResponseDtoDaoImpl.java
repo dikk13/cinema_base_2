ed1d2991af288b86c712c60a39b28deb08e71bf6
Viktor Kollegov <rocket22@yandex.ru>
2022-12-19 22:02:10 +0300

    parameters accounting implemented

--- src/main/java/com/kata/cinema/base/dao/dto/impl/AwardsCeremonyResultResponseDtoDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/dto/impl/AwardsCeremonyResultResponseDtoDaoImpl.java
@@ -19,13 +19,18 @@ public class AwardsCeremonyResultResponseDtoDaoImpl implements AwardsCeremonyRes
 
     @Override
     public List<AwardsCeremonyResultResponseDto> getItemsDto(Integer currentPage, Integer itemsOnPage, Map<String, Object> parameters) {
-        return entityManager.createQuery(" SELECT new com.kata.cinema.base.dto.response.AwardsCeremonyResultResponseDto" +
+        return entityManager.createQuery(" select new com.kata.cinema.base.dto.response.AwardsCeremonyResultResponseDto" +
                                 "(a.id, a.person.id, concat(a.person.firstName, ' ', a.person.lastName), " +
                                 "concat(a.person.originalFirstName, ' ', a.person.originalLastName), a.movie.id, " +
                                 "a.movie.name, a.movie.originalName, a.nomination, a.ceremony.dateEvent, " +
-                                "a.ceremony.id, a.nomination.name, a.nominationStatus) FROM AwardCeremonyResult a"
+                                "a.ceremony.id, a.nomination.name, a.nominationStatus) from AwardCeremonyResult a" +
+                                " where a.ceremony.awards.id = :awardId and a.ceremony.dateEvent = :dateEvent" +
+                                " and a.nominationStatus = :nominationStatus"
                         , AwardsCeremonyResultResponseDto.class)
-                .setFirstResult((currentPage - 1) * itemsOnPage) // нашел аналогичную реализацию на baeldung, но ЗАЧЕМ? ПОЧЕМУ?
+                .setParameter("awardId", parameters.get("awardId"))
+                .setParameter("dateEvent", parameters.get("dateEvent"))
+                .setParameter("nominationStatus", parameters.get("nominationStatus"))
+                .setFirstResult((currentPage - 1) * itemsOnPage)
                 .setMaxResults(itemsOnPage)
                 .getResultList();
     }
@@ -33,7 +38,13 @@ public class AwardsCeremonyResultResponseDtoDaoImpl implements AwardsCeremonyRes
     @Override
     public Long getResultTotal(Map<String, Object> parameters) {
         return jpaResultHelper(entityManager.createQuery("select count (a) " +
-                "from AwardCeremonyResult a", Long.class)).orElse(null);
+                "from AwardCeremonyResult a where a.ceremony.awards.id = :awardId and" +
+                " a.ceremony.dateEvent = :dateEvent and a.nominationStatus = :nominationStatus"
+                , Long.class)
+                .setParameter("awardId", parameters.get("awardId"))
+                .setParameter("dateEvent", parameters.get("dateEvent"))
+                .setParameter("nominationStatus", parameters.get("nominationStatus")))
+                .orElse(null);
     }
 
 }
--- src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/AwardsCeremonyRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/unauthorized/AwardsCeremonyRestController.java
@@ -2,11 +2,17 @@ package com.kata.cinema.base.webapp.controllers.unauthorized;
 
 import com.kata.cinema.base.dto.PageDto;
 import com.kata.cinema.base.dto.response.AwardsCeremonyResultResponseDto;
+import com.kata.cinema.base.models.enums.NominationStatus;
 import com.kata.cinema.base.service.dto.PaginationDtoService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -19,8 +25,14 @@ public class AwardsCeremonyRestController {
     @GetMapping("/ceremonies/result?(optional=false)awardId={awardId}&" +
             "(optional=false)dateEvent={dateEvent}&" +
             "(optional=false)nominationStatus={nominationStatus}")
-    public PageDto<AwardsCeremonyResultResponseDto> getAwardsCeremonyResult() {
-        return paginationDtoService.getPageDto(1, 50);
+    public PageDto<AwardsCeremonyResultResponseDto> getAwardsCeremonyResult(
+            @RequestParam("awardId") Long awardId, @RequestParam("dateEvent") LocalDate dateEvent,
+            @RequestParam("nominationStatus") NominationStatus nominationStatus) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("awardId", awardId);
+        parameters.put("dateEvent", dateEvent);
+        parameters.put("nominationStatus", nominationStatus);
+        return paginationDtoService.getPageDtoWithParameters(1, 50, parameters);
     }
 
 }
