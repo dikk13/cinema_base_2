9ddff82d67bc84f576c6dc246b10d8b5259857a1
ValSipkin <valsipkin@icloud.com>
2023-01-12 15:12:33 +0300

    121-CountriesRestController-tests-1

---
+++ src/test/resources/CountriesRestController/data.sql
@@ -0,0 +1,5 @@
+insert into country (id, name) values ('100', 'USA'),
+                                      ('101', 'Russia'),
+                                      ('102', 'France'),
+                                      ('103', 'Canada'),
+                                      ('104', 'China');
