da6b62d711a8544747d84bf954aa07869cdb2767
ValeriyChurlyaev <chyrlyaevva@gmail.com>
2023-01-16 12:46:13 +0300

    removed @Transactional from DAO into Service, change boolean request(add count)

--- src/main/java/com/kata/cinema/base/dao/entity/Impl/MediaFranchiseDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/entity/Impl/MediaFranchiseDaoImpl.java
@@ -3,19 +3,18 @@ package com.kata.cinema.base.dao.entity.Impl;
 import com.kata.cinema.base.dao.entity.MediaFranchiseDao;
 import com.kata.cinema.base.models.MediaFranchise;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public class MediaFranchiseDaoImpl extends AbstractDaoImpl<Long, MediaFranchise> implements MediaFranchiseDao {
 
     @Override
-    @Transactional
     public Boolean existByName(String name) {
 
 
-        if (entityManager.createQuery("select m.name from MediaFranchise m where m.name =: name")
+        if ((Integer)entityManager.createQuery("select count (m.name) from MediaFranchise m where m.name =: name")
                 .setParameter("name", name)
-                .getSingleResult() != null) {
+                .getSingleResult() > 1) {
             return true;
         } else {
             return false;
